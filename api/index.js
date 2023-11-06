8d6a35cc015e1283cf148bede48143cffb4d814c
Medea Wang <medea.wangxy@gmail.com>
2023-11-06 11:37:13 -0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connect to MongooseDB!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
