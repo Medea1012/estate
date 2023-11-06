d9cf1dd7761b143efdfdadc359157436c6bd77b5
Medea Wang <medea.wangxy@gmail.com>
2023-11-06 17:04:16 -0500

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
@@ -13,6 +14,7 @@ mongoose
     console.log(error);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server running on Port 3000");
@@ -20,3 +22,4 @@ app.listen(3000, () => {
 
 // Path api/index.js
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
