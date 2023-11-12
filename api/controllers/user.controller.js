37a236c41ff3f0976b9c4cfbd150acdef7f112f4
Medea Wang <medea.wangxy@gmail.com>
2023-11-12 16:32:41 -0500

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,37 @@
-export const text = (req, res) => {
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+export const test = (req, res) => {
   res.json({ message: "Api route is working!" });
+};
+
+export const updateUser = async (req, res) => {
+  if (req.user.id !== req.params.id) {
+    return res.status(401).json("You can only update your own account!");
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Something wrong on updating user..." });
+  }
 };
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return res.status(401).json("Unauthorized");
+  }
+  try {
+    const decodedUser = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedUser;
+    next();
+  } catch (error) {
+    return res.status(401).json("Invalid Cookie, please login.");
+  }
+};
