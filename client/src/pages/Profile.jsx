936ef37bd340f74aece4addd1382e50e5cc85c36
Medea Wang <medea.wangxy@gmail.com>
2023-11-14 22:39:07 -0500

    Complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -30,6 +30,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   // firebase storage
@@ -113,6 +115,19 @@ export default function Profile() {
       })
       .catch((error) => {
         dispatch(signOutUserFailure(error?.response?.data?.message));
+        return;
+      });
+  };
+
+  const handleShowListings = async () => {
+    setShowListingsError(false);
+    axios
+      .get(`/api/user/listings/${currentUser._id}`, formData)
+      .then((res) => {
+        setUserListings(res.data);
+      })
+      .catch((error) => {
+        setShowListingsError(error?.response?.data?.message);
         return;
       });
   };
@@ -201,6 +216,51 @@ export default function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="className='h-16 w-16 object-contain'"
+                />
+              </Link>
+              <Link to={`/listing/${listing._id}`}>
+                <p className='text-slate-700 font-semibold hover:underline truncate flex-1"'>
+                  {listing.name}
+                </p>
+              </Link>
+              <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
