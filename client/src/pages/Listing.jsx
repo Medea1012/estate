a1117d90fe5d6959cff5e0fb4a561128ceff4790
Medea Wang <medea.wangxy@gmail.com>
2023-11-19 15:04:17 -0500

    add image slider to the listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { useSelector } from "react-redux";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListing = async () => {
+      setLoading(true);
+      axios
+        .get(`/api/listing/get/${params.listingId}`)
+        .then((res) => {
+          setListing(res.data);
+          setLoading(false);
+          setError(false);
+        })
+        .catch((error) => {
+          setError(true);
+          setLoading(false);
+          console.log(error?.response?.data?.message);
+          return;
+        });
+    };
+    fetchListing();
+  }, [params.listingId]);
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
+}
